fcbc39a27de3044a55c6d99643d5fe7648fee64b
jgliner <jarrett.gliner@gmail.com>
2016-02-03 19:06:59 -0800

    Implement promise and return index.html file

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,28 @@
+var fs = require('fs');
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
-// require more modules/folders here!
+var serve = require("./http-helpers.js");
+var request = require('request');
+var Promise = require('bluebird');
+// serveAssets = function(res, asset, callback)
+// require more modules/folders here! 
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  new Promise(function(resolve, reject) {
+    fs.readFile(archive.paths.siteAssets+'/index.html', 'utf8', function(err, data){
+      if (err) {
+        reject(err);
+      }
+      else {
+        resolve(data);
+      }
+    });
+  })
+  .then(function(data) {
+    res.writeHead(200, serve.headers);
+    res.write(data);
+    res.end();
+  });
+  // console.log(result);
+
 };
